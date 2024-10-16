f67e5ce774942820d2c36d5622b3b08585481eb2
IdirYah <mi_yahiaoui@esi.dz>
2024-10-16 12:00:32 +0200

    client update successfully

--- src/api/controllers/clientController.ts
+++ src/api/controllers/clientController.ts
@@ -2,7 +2,8 @@ import { Request,Response } from "express";
 import mongoose from "mongoose";
 import { StatusCodes } from "http-status-codes";
 import Artisan from "../../db/models/artisan";
-import Client from "../../db/models/client"
+import Client from "../../db/models/client";
+import bcrypt from "bcrypt"
 
 export const getArtisan = async(req:Request,res:Response):Promise<any>=>{
     try{
@@ -48,7 +49,31 @@ export const updateClient = async(req:Request,res:Response):Promise<any>=>{
 
 export const updatePassword = async(req:Request,res:Response):Promise<any>=>{
     try{
-        
+        const {oldPassword,newPassword,confirmPassword} = req.body
+        const idClient:string = req.user?.idClient as string
+        if(!oldPassword || !newPassword || !confirmPassword){
+            return res.status(StatusCodes.BAD_REQUEST).json({message:"Please enter your credenials"})
+        }
+        if(!idClient){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"Client doesn't exists"})
+        }
+        const idClientObjet = new mongoose.Types.ObjectId(idClient)
+        const oldClient = await Client.findById(idClientObjet,)
+        if(!oldClient){
+            return res.status(StatusCodes.NOT_FOUND).json({message:"Client doesn't exists"})
+        }
+        const isMatch = await bcrypt.compare(oldPassword,oldClient.mdp)
+        if(!isMatch){
+            return res.status(StatusCodes.UNAUTHORIZED).json({message:"Invalid Password"})
+        }
+        if(newPassword !== confirmPassword){
+            return res.status(StatusCodes.UNAUTHORIZED).json({message:"Check the new pasword"})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashMDP = await bcrypt.hash(newPassword,salt) as string
+        oldClient.mdp = hashMDP
+        await oldClient.save()
+        res.status(StatusCodes.OK).json({message:"Password updated successfully"})
     }catch(error){
         console.log("Il y a une erreur",error)
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message:"Internal server error"})
--- src/api/routes/clientRoute.ts
+++ src/api/routes/clientRoute.ts
@@ -1,10 +1,11 @@
 import express from "express"
-import { getArtisan,updateClient } from "../controllers/clientController"
+import { getArtisan,updateClient,updatePassword } from "../controllers/clientController"
 import authClient from "../middleware/authClient"
 
 const clientRoutes = express.Router()
 
 clientRoutes.get("/getArtisan",authClient,getArtisan)
 clientRoutes.put("/updateClient",authClient,updateClient)
+clientRoutes.put("/updateClientPassword",authClient,updatePassword)
 
 export default clientRoutes
